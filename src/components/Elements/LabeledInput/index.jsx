4c8a209fad6b5445ca3850f87dcd54e47f175c20
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-12-25 13:39:22 +0700

    update FormSignIn

--- src/components/Elements/LabeledInput/index.jsx
+++ src/components/Elements/LabeledInput/index.jsx
@@ -1,15 +1,21 @@
 import Input from "./Input";
 import Label from "./Label";
 
-const LabeledInput = (props) => {
-  const { label, name, type, placeholder } = props;
-
+const LabeledInput = ({ label, name, type, placeholder, register }) => {
+  
   return (
     <>
       <Label htmlFor={name}>{label}</Label>
-      <Input name={name} type={type} placeholder={placeholder} />
+      <Input 
+        name = {name}
+        type = {type}
+        placeholder = {placeholder}
+        register = {register}
+      />
     </>
   );
+
 };
+
 
 export default LabeledInput;
--- src/components/Fragments/FormSignIn.jsx
+++ src/components/Fragments/FormSignIn.jsx
@@ -1,17 +1,37 @@
 import Button from "../Elements/Button";
 import CheckBox from "../Elements/CheckBox";
 import LabeledInput from "../Elements/LabeledInput";
+import { useForm } from "react-hook-form";
 
 const FormSignIn = () => {
+  const { register, handleSubmit, formState: {errors, isValid}, } = useForm({
+    mode: "onChange",
+  });
+
+  const onFormSubmit = (data) => {
+    console.log(data);
+  };
+  const onErrors = (errors) => console.error(errors);
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
       <div className="mb-6">
         <LabeledInput
           label="Email address"
           type="email"
           placeholder="hello@example.com"
           name="email"
+          register={register("email", {
+            required: "Email address is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email address format",
+            },
+          })}
         />
+        {errors?.email && (
+          <div className="text-center text-red-500">{errors.email.message}</div>
+        )}
       </div>
       <div className="mb-6">
         <LabeledInput
@@ -19,13 +39,31 @@ const FormSignIn = () => {
           type="password"
           placeholder="*************"
           name="password"
+          register={register("password", {
+            required: "Password is required",
+          })}
         />
+        {errors?.password && (
+          <div className="text-center text-red-500">{errors.password.message}</div>
+        )}
       </div>
       <div className="mb-3">
-        <CheckBox label="Keep me signed in" name="status" />
+        <CheckBox
+          label="Keep me signed in"
+          name="status"
+          register={register("status")}
+        />
       </div>
-      <Button variant="bg-primary w-full text-white" type="submit">
-        Login
+      <Button 
+        variant={
+          !isValid
+          ? "bg-gray-05 w-full text-white"
+          : "bg-primary w-full text-white"
+        }
+        type="submit"
+        disabled={!isValid ? "disabled" : ""}
+        >
+          Login
       </Button>
     </form>
   );
