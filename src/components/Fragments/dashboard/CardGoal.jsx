3dbfdf8ac03c3c475968625e271bd244d75bb11f
muhammadalifalhusain <muhalifalhusain@gmail.com>
2024-12-30 20:24:56 +0700

    get data card from backend API

--- src/components/Fragments/dashboard/CardGoal.jsx
+++ src/components/Fragments/dashboard/CardGoal.jsx
@@ -1,10 +1,59 @@
 import { goals } from "../../../data/goals";
+import { useState, useEffect } from "react";
 import Card from "../../Elements/Card";
 import CompositionExample from "../../Elements/GaugeChart";
 import { Icon } from "../../Elements/Icon";
+import axios from "axios";
 
 const CardGoal = () => {
+
+    const [goals, setGoals] = useState({presentAmount: 0, targetAmount: 0});
     const chartValue = goals.presentAmount * 100 / goals.targetAmount;
+
+    const value = (goals.targetAmount * 100) / goals.presentAmount;
+
+    const getData = async () => {
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+  
+        const response = await axios.get(
+          "https://jwt-auth-eight-neon.vercel.app/goals",
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          }
+        );
+  
+        setGoals({
+          presentAmount: response.data.data[0].presentAmount,
+          targetAmount: response.data.data[0].targetAmount,
+        });
+      } catch (error) {
+        if (error.response) {
+          if (error.response.status == 401) {
+            setOpen(true);
+            setMsg({
+              severity: "error",
+              desc: "Session Has Expired. Please Login.",
+            });
+        
+            setIsLoggedIn(false);
+            setName("");
+        
+            localStorage.removeItem("refreshToken");
+            navigate("/login");
+          } else {
+            console.log(error.response);
+          }
+        }
+      }
+    }; 
+
+    useEffect(() => {
+      getData();
+    }, []);
+
   return (
     <Card
       title="Goals"
